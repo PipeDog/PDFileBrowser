23d0a7cfcc61a043e36b5599706d26be83d7c2a1
liang <18811130435@163.com>
2020-01-04 21:03:17 +0800

    commit

--- PDFileBrowser/Source/PDFilePreviewController.m
+++ PDFileBrowser/Source/PDFilePreviewController.m
@@ -10,9 +10,10 @@
 #import <WebKit/WebKit.h>
 #import "PDFileBrowser.h"
 
-@interface PDFilePreviewController ()
+@interface PDFilePreviewController () <WKNavigationDelegate>
 
 @property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicatorView;
 
 @end
 
@@ -26,6 +27,7 @@
 
 - (void)createViewHierarchy {
     [self.view addSubview:self.webView];
+    [self.webView addSubview:self.activityIndicatorView];
 }
 
 #pragma mark - PDFilePreviewControllerDelegate Methods
@@ -39,20 +41,57 @@
         NSURLRequest *request = [NSURLRequest requestWithURL:URL];
         [self.webView loadRequest:request];
     }
-    
+
     self.title = URL.absoluteString;
+}
+
+#pragma mark - WKNavigationDelegate
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    self.activityIndicatorView.hidden = NO;
+    [self.activityIndicatorView startAnimating];
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+    [self.activityIndicatorView stopAnimating];
+    self.activityIndicatorView.hidden = YES;
+    
+    // Handle error...
+    NSLog(@"error: %@", error);
+    
+    self.navigationItem.prompt = @"Error";
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    [self.activityIndicatorView stopAnimating];
+    self.activityIndicatorView.hidden = YES;
 }
 
 #pragma mark - Getter Methods
 - (WKWebView *)webView {
     if (!_webView) {
         WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
-        [configuration.preferences setValue:@YES forKey:@"allowFileAccessFromFileURLs"];
         
         _webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:configuration];
         _webView.backgroundColor = [UIColor whiteColor];
+        _webView.allowsLinkPreview = YES;
+        _webView.navigationDelegate = self;
     }
     return _webView;
+}
+
+- (UIActivityIndicatorView *)activityIndicatorView {
+    if (!_activityIndicatorView) {
+        _activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        _activityIndicatorView.hidden = YES;
+        
+        CGSize size = CGSizeMake(100.f, 100.f);
+        CGRect rect = CGRectMake((CGRectGetWidth(self.view.bounds) - size.width) / 2.f,
+                                 (CGRectGetHeight(self.view.bounds) - size.height) / 2.f,
+                                 size.width,
+                                 size.height);
+        _activityIndicatorView.frame = rect;
+    }
+    return _activityIndicatorView;
 }
 
 @end
